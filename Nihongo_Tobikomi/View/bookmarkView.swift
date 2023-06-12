6ac499eaad2edc4e564a041da4b82b31b4fe8e6b
kaito <sudoswift@gmail.com>
2023-06-12 20:59:13 +0900

    create View, Model, ViewModel Group / create file mainView, bookmarkView / View, Model, ViewModel グループを作る　／mainView, bookmarkViewを作る

---
+++ Nihongo_Tobikomi/View/bookmarkView.swift
@@ -0,0 +1,20 @@
+//
+//  bookmarkView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 2023/06/12.
+//
+
+import SwiftUI
+
+struct bookmarkView: View {
+    var body: some View {
+        Text("お気に入り")
+    }
+}
+
+struct bookmarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        bookmarkView()
+    }
+}
---
+++ Nihongo_Tobikomi/View/mainView.swift
@@ -0,0 +1,20 @@
+//
+//  mainView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 2023/06/12.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    var body: some View {
+        Text("ナウView")
+    }
+}
+
+struct mainView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainView()
+    }
+}

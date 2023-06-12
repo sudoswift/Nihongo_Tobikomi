62dfe36d8b76959b1279fd0c2471366e205d1213
kaito <sudoswift@gmail.com>
2023-06-12 19:33:57 +0900

    Initial Commit

---
+++ Nihongo_Tobikomi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 2023/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nihongo_Tobikomi/Nihongo_TobikomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nihongo_TobikomiApp.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct Nihongo_TobikomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

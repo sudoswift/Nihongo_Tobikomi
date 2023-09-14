1bf45b876fd0b479cbf2e5b3f11a531632c43b16
kaito <sudoswift@gmail.com>
2023-09-14 13:28:20 +0900

    [add] mainView toolbargroup add Sf Symbols

--- Nihongo_Tobikomi/View/mainView.swift
+++ Nihongo_Tobikomi/View/mainView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct mainView: View {
     var body: some View {
-        Text("ナウView")
+        NavigationStack {
+            Text("NOW VIEW")
+                .navigationTitle("ソヒョン")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing){
+                        Button {
+                            print("alaram")
+                        } label: {
+                            Image(systemName: "bell")
+                        } //alaram Button
+                        .tint(.black)
+                        Button {
+                            print("ranking")
+                        } label: {
+                            Image(systemName: "crown")
+                        } //ranking Button
+                        .tint(.black)
+                        Button {
+                            print("setting")
+                        } label: {
+                            Image(systemName: "gearshape")
+                        } //setting Button
+                        .tint(.black)
+                    }
+                }
+        }//NavigationStack
     }
 }
 

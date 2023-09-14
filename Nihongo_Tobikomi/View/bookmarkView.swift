36742e8a89460f042c87e30c9d4f76936eef70cc
kaito <sudoswift@gmail.com>
2023-09-14 15:10:20 +0900

    [add] bookmarkView add rireki

--- Nihongo_Tobikomi/View/bookmarkView.swift
+++ Nihongo_Tobikomi/View/bookmarkView.swift
@@ -10,26 +10,44 @@ import SwiftUI
 struct bookmarkView: View {
     var body: some View {
         NavigationStack {
-            Text("お気に入り")
-                .navigationBarTitle("お気に入り", displayMode: .inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing){
-                        Button {
-                            print("setting")
-                        } label: {
-                            Image(systemName: "gearshape")
-                        } //setting Button & sheetView 예정
-                        .tint(.black)
+            VStack{
+                VStack {
+                    HStack {
+                        Image(systemName: "clock.arrow.circlepath")
+                        Text("履歴")
+                        Spacer()
+                    }//HStack
+                    .padding()
+                    HStack{
+                        Text("飛び込む")
+                        Text("뛰어들다")
                     }
-                    ToolbarItem(placement: .navigationBarLeading){
-                        Button {
-                            print("setting")
-                        } label: {
-                            Image(systemName: "flame")
-                        } //setting Button & sheetView 예정
-                        .tint(.black)
+                    HStack{
+                        Text("呼び出す")
+                        Text("호출하다")
                     }
-                }//toolbar
+                }//履歴VStack
+                // N1~N2 는 List로 만들기
+            }//VStack
+            .navigationBarTitle("お気に入り", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button {
+                        print("setting")
+                    } label: {
+                        Image(systemName: "gearshape")
+                    } //setting Button & sheetView 예정
+                    .tint(.black)
+                }
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button {
+                        print("setting")
+                    } label: {
+                        Image(systemName: "flame")
+                    } //setting Button & sheetView 예정
+                    .tint(.black)
+                }
+            }//toolbar
         }
     }
 }

ea2d924880731c44c5ab451d2858b4b484da0b8b
kaito <sudoswift@gmail.com>
2023-10-03 20:48:56 +0900

    [add] UserViewModel base information using @Published

--- Nihongo_Tobikomi/ViewModel/UserViewModel.swift
+++ Nihongo_Tobikomi/ViewModel/UserViewModel.swift
@@ -9,3 +9,28 @@ import Foundation
 import FirebaseAuth
 import Firebase
 import FirebaseStorage
+
+class UserViewModel: ObservableObject{
+    //MARK: - Login
+    @Published var emailID: String = ""
+    //MARK: - Current User
+    @Published var currentUser: Firebase.User?
+    //MARK: - Register
+    @Published var registerEmail: String = ""
+    @Published var userName: String = ""
+    //MARK: - Fetch Current User's Data
+    @Published var currentUserData: User?
+    //MARK: - UserDefaults Log Status
+    @Published var logStatus: Bool = false{
+        didSet{
+            UserDefaults.standard.set(logStatus, forKey: "logStatus")
+        }
+    }
+    init(){
+        self.logStatus = UserDefaults.standard.bool(forKey: "logStatus")
+    }
+    //MARK:- Fetch Data
+    let database = Firestore.firestore()
+    
+    
+}// class UserViewModel

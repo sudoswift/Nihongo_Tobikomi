9bd3e05e9b222ae3a4de0427f2c0742cfcb9d1d2
kaito <sudoswift@gmail.com>
2024-08-17 22:38:51 +0900

    add Views

---
+++ Nihongo_Tobikomi/View/addWordView.swift
@@ -0,0 +1,18 @@
+//
+//  addWordView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 8/17/24.
+//
+
+import SwiftUI
+
+struct addWordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addWordView()
+}
---
+++ Nihongo_Tobikomi/View/jlptView.swift
@@ -0,0 +1,18 @@
+//
+//  jlptView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 8/17/24.
+//
+
+import SwiftUI
+
+struct jlptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    jlptView()
+}

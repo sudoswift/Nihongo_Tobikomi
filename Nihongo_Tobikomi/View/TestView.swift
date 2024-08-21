5be6e0bacb6ce8278dbc1b4f90520db606ec115f
kaito <sudoswift@gmail.com>
2024-08-21 21:41:56 +0900

    add TextView.swift

---
+++ Nihongo_Tobikomi/View/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Nihongo_Tobikomi
+//
+//  Created by ioio on 8/21/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}

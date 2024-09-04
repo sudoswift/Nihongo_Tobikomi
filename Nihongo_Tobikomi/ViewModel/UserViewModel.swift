8e9fbefda8433e84acd22f3d9bde1ed13fa57e41
kaito <sudoswift@gmail.com>
2024-09-04 14:20:27 +0900

    UserViewModel

--- Nihongo_Tobikomi/ViewModel/UserViewModel.swift
+++ Nihongo_Tobikomi/ViewModel/UserViewModel.swift
@@ -34,33 +34,33 @@ class UserViewModel: ObservableObject {
         }
     }
     
+    //MARK: - Firebase User 컬렉션에서 정보를 가져옴
     private func fetchUserData(uid: String) {
         // 사용자 데이터를 가져오는 로직
     }
     
-    func signIn(email: String, password: String, completion: @escaping (Bool) -> Void) {
-        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+    //MARK: - 로그인
+    func signIn(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
             if let error = error {
                 print("Sign In Error: \(error.localizedDescription)")
-                completion(false)
             } else {
-                self.fetchUserData(uid: result?.user.uid ?? "")
-                completion(true)
+                //Auth.auth().signIn으로 email과 password가 일치 했을 때 로그인이 되고 userUID를 가져와서 fetchUserData에서 uid를 기반으로 유저의 정보를 가져온다.
+                self.fetchUserData(uid: authResult?.user.uid ?? "")
             }
         }
     }
     
-    func signUp(email: String, password: String, userName: String, completion: @escaping (Bool) -> Void) {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+    //MARK: - 회원가입
+    func signUp(email: String, password: String, userName: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
                 print("Sign Up Error: \(error.localizedDescription)")
-                completion(false)
             } else {
-                let uid = result?.user.uid ?? ""
-                let newUser = User(userName: userName, userUID: uid, userEmail: email)
-                self.saveUserData(user: newUser)
+                let uid = authResult?.user.uid ?? "" // uid 생성
+                let newUser = User(userName: userName, userUID: uid, userEmail: email) //newUser에 회원가입 화면에서 입력받은 userName, email과 윗줄의 uid를 저장함
+                self.saveUserData(user: newUser) //saveUserData를 이용해 firebase에 user 정보를 입력함(userName, userEmail, userUID)
                 self.user = newUser
-                completion(true)
             }
         }
     }
@@ -69,5 +69,3 @@ class UserViewModel: ObservableObject {
         // 사용자 데이터를 저장하는 로직
     }
 }
-
-
